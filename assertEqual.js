be20c49aa0347b465a725a180a082a9d9ec367b5
d0ugr <doug@escm.ml>
2020-03-02 20:10:36 +0000

    Modified assertion strings to use template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,11 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
 
+  // +++ and --- used for fonts without emojis:
   if (actual === expected) {
-    console.log("Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ +++ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 --- Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
