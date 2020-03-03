623f664b5cccfbf932eed9d0e24044971f12ed3e
d0ugr <doug@escm.ml>
2020-03-03 22:44:56 +0000

    Created skeleton without()

---
+++ without.js
@@ -0,0 +1,38 @@
+const eqArrays = function(array1, array2) {
+
+  if (array1.length !== array2.length) return false;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+
+  return true;
+
+};
+
+const assertArraysEqual = function(actual, expected) {
+
+  // +++ and --- used for fonts without emojis:
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ +++ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 --- Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const without = funciton(source, itemsToRemove) {
+
+  let result = [];
+
+  for (i = 0; i < source.length; i++) {
+    for (j = 0; j < itemsToRemove.length; j++) {
+
+    }
+  }
+
+  return result;
+};
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
