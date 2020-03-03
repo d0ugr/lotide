15f045c9eb286c2c844f28c4ef4e8022fb344b01
d0ugr <doug@escm.ml>
2020-03-03 21:57:48 +0000

    Created middle()

---
+++ middle.js
@@ -0,0 +1,34 @@
+const eqArrays = function(array1, array2) {
+
+  if (array1.length !== array2.length) return false;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+
+  return true;
+
+};
+
+const assertArraysEqual = function(actual, expected) {
+
+  // +++ and --- used for fonts without emojis:
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ +++ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 --- Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const middle = function(array) {
+
+  let result = [];
+
+  result.push(array[Math.floor(array.length / 2)]);
+
+  return result;
+
+};
+
+console.log(middle([1, 2, 3]));
