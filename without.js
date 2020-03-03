db3b75b7427242e1bbbad835255f68d196bfcf56
d0ugr <doug@escm.ml>
2020-03-03 23:14:24 +0000

    Added test case to check original array

--- without.js
+++ without.js
@@ -35,6 +35,11 @@ const without = function(source, itemsToRemove) {
 
 };
 
+const words = ["hello", "world", "lighthouse"];
+without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
 assertArraysEqual(without([], []), []);
 assertArraysEqual(without([1], [1]), []);
 assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
