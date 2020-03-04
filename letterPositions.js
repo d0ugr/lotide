b7405a4d344cd4b2e44b9822dc3fa66aa8d296b6
d0ugr <doug@escm.ml>
2020-03-04 19:42:55 +0000

    Initial empty letterPositions() with proper assert functions

--- letterPositions.js
+++ letterPositions.js
@@ -1,7 +1,19 @@
-const assertEqual = function(actual, expected) {
+const eqArrays = function(array1, array2) {
+
+  if (array1.length !== array2.length) return false;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+
+  return true;
+
+};
+
+const assertArraysEqual = function(actual, expected) {
 
   // +++ and --- used for fonts without emojis:
-  if (actual === expected) {
+  if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ +++ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑🛑🛑 --- Assertion Failed: ${actual} !== ${expected}`);
@@ -19,3 +31,4 @@ const letterPositions = function(sentence) {
 
 };
 
+console.log(letterPositions("hello"));
