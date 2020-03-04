f3fb2bfe683a5705f94390228403668bf02ecbb0
d0ugr <doug@escm.ml>
2020-03-04 19:36:22 +0000

    Initial empty letterPositions()

---
+++ letterPositions.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+
+  // +++ and --- used for fonts without emojis:
+  if (actual === expected) {
+    console.log(`✅✅✅ +++ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 --- Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const letterPositions = function(sentence) {
+
+  const results = {};
+
+  // logic to update results here
+
+  return results;
+
+};
+
