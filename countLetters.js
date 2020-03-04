2e37c48e6ac5e724aaf70fde1a4fb7624cbe2816
d0ugr <doug@escm.ml>
2020-03-04 19:26:41 +0000

    Modified countLetters() to ignore spaces

--- countLetters.js
+++ countLetters.js
@@ -13,7 +13,7 @@ const countLetters = function(inString) {
 
   let result = {};
 
-  for (letter of inString) {
+  for (letter of inString.replace(/ /g, "")) {
     if (!result[letter]) {
       result[letter] = 1;
     } else {
