7f99528da9dbc295288915a9199f0fd1fdceefc1
d0ugr <doug@escm.ml>
2020-03-04 19:06:44 +0000

    countOnly() counts only items in itemsToCount

--- countOnly.js
+++ countOnly.js
@@ -16,11 +16,12 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
-    console.log(item);
-    if (!results[item]) {
-      results[item] = 1;
-    } else {
-      results[item]++;
+    if (itemsToCount[item]) {
+      if (!results[item]) {
+        results[item] = 1;
+      } else {
+        results[item]++;
+      }
     }
   }
 
