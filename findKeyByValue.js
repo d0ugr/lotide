7db44b159cbcc721976327b86dea2fe4a0b9e4b5
d0ugr <doug@escm.ml>
2020-03-04 20:53:39 +0000

    Added test cases

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -21,10 +21,19 @@ const findKeyByValue = function(searchObject, findValue) {
 
 const bestTVShowsByGenre = {
   // eslint complains about sci_fi:
-  sciFi:  "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
+  sciFi:                         "The Expanse",
+  comedy:                        "Brooklyn Nine-Nine",
+  drama:                         "The Wire",
+  methProduction:                "Breaking Bad",
+  mindControl:                   "The Path",
+  chickensThatMeowDocumentaries: ""
 };
 
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sciFi");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Breaking Bad"), "methProduction");
+assertEqual(findKeyByValue(bestTVShowsByGenre, ""), "chickensThatMeowDocumentaries");
+
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "the path"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "1820992 nhd932dhx8b  4gqfg4u3k, 9 \"go9oohceho9cewbgouc42 iul\""), undefined);
