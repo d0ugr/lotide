9f33e58980dbbdd109b20185b435e9f88125f5d8
d0ugr <doug@escm.ml>
2020-03-04 21:35:31 +0000

    Better array value check in eqObjects()

--- eqObjects.js
+++ eqObjects.js
@@ -27,7 +27,7 @@ const eqObjects = function(object1, object2) {
 
   if (Object.keys(object1).length === Object.keys(object2).length) {
     for (const key in object1) {
-      if (!Array.isArray(object1[key])) {
+      if (!Array.isArray(object1[key]) && !Array.isArray(object2[key])) {
         if (object1[key] !== object2[key]) return false;
       } else {
         if (!eqArrays(object1[key], object2[key])) return false;
