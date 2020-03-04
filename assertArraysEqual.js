d39bbdb401f0a2e3a0bd9e76a6ebe4f4b34bb706
d0ugr <doug@escm.ml>
2020-03-04 22:14:07 +0000

    Added check for undefined arrays in eqArrays()

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,6 @@
 const eqArrays = function(array1, array2) {
 
+  if (!array1 || !array2) return false;
   if (array1.length !== array2.length) return false;
 
   for (let i = 0; i < array1.length; i++) {
--- eqArrays.js
+++ eqArrays.js
@@ -11,6 +11,7 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(array1, array2) {
 
+  if (!array1 || !array2) return false;
   if (array1.length !== array2.length) return false;
 
   for (let i = 0; i < array1.length; i++) {
