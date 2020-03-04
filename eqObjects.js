7eb638be2bd5e53707a7ba00ce200bea52c841a5
d0ugr <doug@escm.ml>
2020-03-04 21:11:15 +0000

    Initial empty eqObjects() and failing test cases

---
+++ eqObjects.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+
+  // +++ and --- used for fonts without emojis:
+  if (actual === expected) {
+    console.log(`✅✅✅ +++ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 --- Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+
+
+
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false);
