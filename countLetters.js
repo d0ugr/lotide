9d43461ff61bfe15a2d645456cc2417f523267e2
d0ugr <doug@escm.ml>
2020-03-04 19:30:22 +0000

    Added more test cases

--- countLetters.js
+++ countLetters.js
@@ -29,4 +29,7 @@ const testString = "lighthouse in the house";
 console.log(countLetters(testString));
 console.log(testString);
 
+console.log(countLetters(""));
+console.log(countLetters("123"));
+console.log(countLetters("123233"));
 console.log(countLetters("Word to Snuffy"));
