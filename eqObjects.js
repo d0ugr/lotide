c35db231c62679891139eec7a13a217002388c5e
d0ugr <doug@escm.ml>
2020-03-04 21:21:40 +0000

    Added array values test cases

--- eqObjects.js
+++ eqObjects.js
@@ -32,3 +32,10 @@ assertEqual(eqObjects(ab, ba), true);
 
 const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(ab, abc), false);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
