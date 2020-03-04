4d1d6e48f90b60238e39220252838961089b5f45
d0ugr <doug@escm.ml>
2020-03-04 21:18:10 +0000

    Initial eqObjects() implementation

--- eqObjects.js
+++ eqObjects.js
@@ -13,8 +13,17 @@ const assertEqual = function(actual, expected) {
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
 
+  if (Object.keys(object1).length === Object.keys(object2).length) {
+    for (const key in object1) {
+      if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
 
-
+  return true;
 };
 
 const ab = { a: "1", b: "2" };
