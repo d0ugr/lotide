e16ee0e55aedfa23fcde75d3ee545bb0feb5b656
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-05 15:05:34 -0500

    Added test cases

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,27 @@
+const eqArrays = function(array1, array2) {
+
+  if (!array1 || !array2) return false;
+  if (array1.length !== array2.length) return false;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+
+  return true;
+
+};
+
+const assertArraysEqual = function(actual, expected) {
+
+  // +++ and --- used for fonts without emojis:
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ +++ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 --- Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
 const takeUntil = function(array, callback) {
 
   let result = [];
@@ -17,9 +41,18 @@ const takeUntil = function(array, callback) {
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
 
-console.log('---');
+console.log("---");
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
+const results2 = takeUntil(data2, x => x === ",");
 console.log(results2);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
+
+console.log("---");
+
+const data3 = ["Can", "I", "have", "$42,000?"];
+const results3 = takeUntil(data2, x => true);
+console.log(results3);
+assertArraysEqual(results3, []);
