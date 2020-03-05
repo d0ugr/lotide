5161e199622f56793b30b04f67925ebcbfaced0d
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-05 14:39:02 -0500

    Added test case

--- map.js
+++ map.js
@@ -1,3 +1,29 @@
+const eqArrays = function(array1, array2) {
+
+  if (!array1 || !array2) return false;
+  if (array1.length !== array2.length) return false;
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+
+  return true;
+
+};
+
+const assertArraysEqual = function(actual, expected) {
+
+  // +++ and --- used for fonts without emojis:
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ +++ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 --- Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+
 const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function(array, callback) {
@@ -14,3 +40,4 @@ const map = function(array, callback) {
 
 const results1 = map(words, word => word[0]);
 console.log(results1);
+assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
