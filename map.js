3c03464cf828a6f381bd1d402bf901e4da1aad47
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-05 13:41:06 -0500

    Added map parameters

--- map.js
+++ map.js
@@ -1,6 +1,10 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
-const map = function() {
+const map = function(array, callback) {
+
+  // temporary code:
+  console.log('array: ', array);
+  console.log('callback: ', callback);
 
   const results = [];
   return results;
