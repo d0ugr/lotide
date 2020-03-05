c17a9ce321c21c52ecf15abee49a0d4c7ea01baf
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-05 15:30:16 -0500

    Added test cases

--- findKey.js
+++ findKey.js
@@ -17,11 +17,16 @@ const findKey = function(object, callback) {
 
 };
 
-assertEqual(findKey({
+let data = {
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma");
+  "Akelarre":  { stars: 4 }
+};
+
+assertEqual(findKey(data, x => x.stars === 5), undefined);
+assertEqual(findKey(data, x => x.stars === 2), "noma");
+assertEqual(findKey(data, x => x.stars === 4), "Akelarre");
+assertEqual(findKey(data, x => x.stars >= 3), "Akaleri");
