6c63fb9dd61e833740e0731c202b726547aea5fb
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-06 16:29:30 -0500

    Added test cases

--- eqArrays.js
+++ eqArrays.js
@@ -51,7 +51,17 @@ assertEqual(eqArrays([{}], [{}, {}]), false);
 
 console.log("W01D5 - Recursive eqArrays");
 
+assertEqual(eqArrays([[[[1]]]], [[[[1]]]]), true);
+assertEqual(eqArrays([[[[1, 2]]]], [[[[1, 2]]]]), true);
+assertEqual(eqArrays([1, [2, 3], [4]], [1, [2, 3], [4]]), true);
+assertEqual(eqArrays([1, [2, 3], [4], [], []], [1, [2, 3], [4], [], []]), true);
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4], [5, [6, [7]]]], [[2, 3], [4], [5, [6, [7]]]]), true);
 
+assertEqual(eqArrays([[[[1]]]], [[[1]]]), false);
+assertEqual(eqArrays([[[[1, 2]]]], [[[1, 2]]]), false);
+assertEqual(eqArrays([1, [2, 3], [4]], [1, [2, 3]]), false);
+assertEqual(eqArrays([1, [2, 3], [4], [], []], [1, [2, 3], [4], []]), false);
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
 assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+assertEqual(eqArrays([[2, 3], [4], [5, [6, [7]]]], [[2, 3], [4], [5, [6]]]), false);
