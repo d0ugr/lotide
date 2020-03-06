9ad93547db1a52778e3cf9727fcfe6d91c59333d
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-06 15:32:01 -0500

    Modified eqObjects for recursion

--- eqObjects.js
+++ eqObjects.js
@@ -29,7 +29,11 @@ const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length === Object.keys(object2).length) {
     for (const key in object1) {
       if (!Array.isArray(object1[key]) && !Array.isArray(object2[key])) {
-        if (object1[key] !== object2[key]) return false;
+        if (typeof object1[key] !== "object" && typeof object2[key] !== "object") {
+          if (object1[key] !== object2[key]) return false;
+        } else {
+          return eqObjects(object1[key], object2[key]);
+        }
       } else {
         if (!eqArrays(object1[key], object2[key])) return false;
       }
