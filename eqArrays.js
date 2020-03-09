7662211bffdc8f14c191c2ce3783bd131b97355d
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-09 18:06:40 -0400

    Refactored letterPositions and its test cases

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,3 @@
-const assertEqual = require("./assertEqual");
-
 const eqArrays = function(array1, array2) {
 
   if (!array1 || !array2) return false;
---
+++ test/letterPositionsTest.js
@@ -0,0 +1,66 @@
+const assert          = require("chai").assert;
+const letterPositions = require("../letterPositions");
+
+describe("#letterPositions", () => {
+
+  let result;
+
+  result = letterPositions("hello");
+
+  it("result -> { h: [ 0 ], e: [ 1 ], l: [ 2, 3 ], o: [ 4 ] }", () => {
+    assert(result, { h: [ 0 ], e: [ 1 ], l: [ 2, 3 ], o: [ 4 ] });
+  });
+  it("result[\" \"] -> undefined", () => {
+    assert.isUndefined(result[" "]);
+  });
+  it("result.h -> [0]", () => {
+    assert(result.h, [0]);
+  });
+  it("result.e -> [1]", () => {
+    assert(result.e, [1]);
+  });
+  it("result.l -> [2, 3]", () => {
+    assert(result.l, [2, 3]);
+  });
+  it("result.o -> [4]", () => {
+    assert(result.o, [4]);
+  });
+
+  result = letterPositions("lighthouse in the house");
+
+  console.log(result);
+  it("result[\" \"] -> undefined", () => {
+    assert.isUndefined(result[" "]);
+  });
+  it("result.l -> [0]", () => {
+    assert(result.l, [0]);
+  });
+  it("result.i -> [1, 11])", () => {
+    assert(result.i, [1, 11]);
+  });
+  it("result.g -> [2]", () => {
+    assert(result.g, [2]);
+  });
+  it("result.h -> [3, 5, 15, 18]", () => {
+    assert(result.h, [3, 5, 15, 18]);
+  });
+  it("result.t -> [4, 14]", () => {
+    assert(result.t, [4, 14]);
+  });
+  it("result.o -> [6, 19]", () => {
+    assert(result.o, [6, 19]);
+  });
+  it("result.u -> [7, 20]", () => {
+    assert(result.u, [7, 20]);
+  });
+  it("result.s -> [8, 21]", () => {
+    assert(result.s, [8, 21]);
+  });
+  it("result.e -> [9, 16, 22]", () => {
+    assert(result.e, [9, 16, 22]);
+  });
+  it("result.n -> [12]", () => {
+    assert(result.n, [12]);
+  });
+
+});
