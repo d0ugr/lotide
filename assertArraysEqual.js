be43893e68e944df95fb9d3dd45d55f40d6ea9a9
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-09 11:54:59 -0400

    Removed dumbass emojis

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -4,9 +4,9 @@ const assertArraysEqual = function(actual, expected) {
 
   // +++ and --- used for fonts without emojis:
   if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ +++ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`+++ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑 --- Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`--- Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
